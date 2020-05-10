5773c18273925ba9e51d7b0f43e652bbf3b2e4da
FixxxarN <tornstromandre@gmail.com>
2020-05-10 22:58:07 +0200

    Added a player and made him move and jump with groundCheck

---
+++ survival/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float jumpHeight = 5f;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private Rigidbody2D rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpHeight);
+        }
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.velocity = new Vector2(-movementSpeed, rigidbody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.velocity = new Vector2(movementSpeed, rigidbody.velocity.y);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+        }
+    }
+}
